e0eb43c53f59a083b82dd410bcc819873fc363ae
tamagochita <mevere66@gmail.com>
2022-04-07 20:10:25 -0500

    modificacion en dependencias

---
+++ src/main/java/com/example/testingReactiveStreams/TestingReactiveStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingReactiveStreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingReactiveStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingReactiveStreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testingReactiveStreams/TestingReactiveStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingReactiveStreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingReactiveStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
